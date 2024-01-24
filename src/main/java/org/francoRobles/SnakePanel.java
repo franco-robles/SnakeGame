da34e82947e76dfed3daac5c48f8fb34c8cb5907
franco_robles <franco_robles94@outlook.com>
2024-01-24 15:34:18 -0300

    add snake and colicion

--- src/main/java/org/francoRobles/SnakePanel.java
+++ src/main/java/org/francoRobles/SnakePanel.java
@@ -13,14 +13,14 @@ public class SnakePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
-    static final int DELAY = 75;
+    static final int DELAY = 50;
     final int x[] =  new int[GAME_UNITS];
     final int y[] =  new int[GAME_UNITS];
     int bodyParts = 6;
     int applesEatens;
     int appleX;
     int appleY;
-    char direction = 'R'; //R, L, D, U
+    Direction direction = Direction.Right;
     Timer timer;
     boolean running = false;
     Random random;
@@ -57,6 +57,18 @@ public class SnakePanel extends JPanel implements ActionListener {
         }
         g.setColor(Color.RED);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < this.bodyParts; i++) {
+            if (i==0){
+                g.setColor(Color.GREEN);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }else{
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+
+
     }
 
     public void newApple(){
@@ -65,6 +77,16 @@ public class SnakePanel extends JPanel implements ActionListener {
     }
 
     public void move(){
+        for (int i = this.bodyParts; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch (direction){
+            case UP -> y[0] = y[0]-UNIT_SIZE;
+            case Down -> y[0] = y[0]+UNIT_SIZE;
+            case Left -> x[0] = x[0]-UNIT_SIZE;
+            case Right -> x[0] = x[0]+UNIT_SIZE;
+        }
 
     }
 
@@ -73,20 +95,36 @@ public class SnakePanel extends JPanel implements ActionListener {
     }
 
     public void checkCollision(){
-
+        //check if head collides with body
+        for(int i = this.bodyParts; i < 0; i--){
+            if (x[0]==x[i] && y[0]==y[i]){
+                running =  false;
+            }
+        }
     }
 
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(this.running){
+            move();
+            checkApple();
+            checkCollision();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
-
+            char ch = e.getKeyChar();
+            switch (ch){
+                case  'w' -> direction = Direction.UP;
+                case  's' -> direction = Direction.Down;
+                case  'a' -> direction = Direction.Left;
+                case  'd' -> direction = Direction.Right;
+            }
         }
     }
 
